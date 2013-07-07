c27638ce38ee20c86597459fa5f8d98a8dc7c3d7
Jxck <block.rxckin.beats@gmail.com>
2013-07-07 10:23:01 +0900

    add Client & Read(), Write()

--- simple/main.go
+++ simple/main.go
@@ -2,18 +2,53 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
-	"io"
+	"fmt"
+	"log"
 	"net/http"
 )
 
+type Client struct {
+	ws *websocket.Conn
+}
+
+func (c *Client) Read() string {
+	data := make([]byte, 512)
+	n, err := c.ws.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(data[:n])
+}
+
+func (c *Client) Write(data string) int {
+	n, err := c.ws.Write([]byte(data))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return n
+}
+
+func NewClient(ws *websocket.Conn) *Client {
+	client := &Client{
+		ws: ws,
+	}
+	return client
+}
+
 func echoHandler(ws *websocket.Conn) {
-	io.Copy(ws, ws)
+	client := NewClient(ws)
+
+	message := client.Read()
+	fmt.Printf("Receive: %s\n", message)
+
+	client.Write(message)
+	fmt.Printf("Send: %s\n", message)
 }
 
 func main() {
 	http.Handle("/echo", websocket.Handler(echoHandler))
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
