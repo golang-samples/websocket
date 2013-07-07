f2d4a745774e5ce19b88dc8e0708bf065f0fa781
Jxck <block.rxckin.beats@gmail.com>
2013-07-07 09:23:49 +0900

    revert simple/main.go using io.Copy

--- simple/main.go
+++ simple/main.go
@@ -2,24 +2,12 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
-	"fmt"
-	"log"
+	"io"
 	"net/http"
 )
 
 func echoHandler(ws *websocket.Conn) {
-	msg := make([]byte, 512)
-	n, err := ws.Read(msg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Receive: %s\n", msg[:n])
-
-	m, err := ws.Write(msg[:n])
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Send: %s\n", msg[:m])
+	io.Copy(ws, ws)
 }
 
 func main() {
